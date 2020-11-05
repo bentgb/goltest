b7aa5ac049d1df8e53beac9d651c145e8216bd3b
Aisha Al-safi <marena665@hotmail.com.com>
2020-11-05 17:34:03 +0100

    Refactor

--- src/main/java/org/example/Rules.java
+++ src/main/java/org/example/Rules.java
@@ -2,15 +2,28 @@ package org.example;
 
 public class Rules {
     public Status nextGeneration(Status cell, Neighbors neighbors) {
-        if (cell == Status.ALIVE) {
-            if (neighbors.getValue() < 2 || neighbors.getValue() > 3) {
+        if (cell == Status.ALIVE)
+        {  if (underPopulated(neighbors) || overPopulated(neighbors)){
                 cell = Status.DEAD;
             }
-        }git
-        else if (neighbors.getValue() == 3){
+            }
+        else if (deadCellBecomesAliveCell(neighbors)){
                 cell = Status.ALIVE;
             }
         return cell;
 
+    }
+
+    private boolean deadCellBecomesAliveCell(Neighbors neighbors) {
+        return neighbors.getValue() == 3;
+
+    }
+
+    private boolean overPopulated(Neighbors neighbors) {
+        return neighbors.getValue() >3;
+    }
+
+    private boolean underPopulated(Neighbors neighbors) {
+        return neighbors.getValue() < 2;
     }
 }

8b2ab90c6bab7beddf452af316414a057e39ceb1
Tugba A <karaca.tgb@gmail.com>
2020-11-05 21:05:02 +0100

    mistakenly deleted Aishas tests, added again.

--- src/test/java/org/example/RulesTest.java
+++ src/test/java/org/example/RulesTest.java
@@ -32,6 +32,18 @@ public class RulesTest {
 
     }
 
+    @Test
+    public  void deadCellWithTwoNeighborsDoNotResurrect(){
+        Assert.assertEquals(deadCellWith(Neighbors.TWO_NEIGHBORS), Status.DEAD);
+
+    }
+
+    @Test
+    public  void deadCellWithFourNeighborsDoNotResurrect(){
+        Assert.assertEquals(deadCellWith(Neighbors.FOUR_NEIGHBORS), Status.DEAD);
+
+    }
+
     private Status liveCellWith(Neighbors neighbors){
         return nextGeneration(Status.ALIVE, neighbors);
     }
